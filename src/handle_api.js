b85274b89dfdc092b04218e3fd9654779eb9feb4
justTrueCodeWriter <dmjustcoder8@gmail.com>
2024-03-23 12:02:50 +0300

    upd

--- src/handle_api.js
+++ src/handle_api.js
@@ -1,6 +1,15 @@
+//const { endAsyncEvent } = require("react-native/Libraries/Performance/Systrace");
 
-export function getEndpointCurrencyAmount(base_currency, endpoint_currency) {
-    let response = fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${base_currency}.json`)
-    let data = response.json()
-    return data[`${endpoint_currency}`]
+async function getEndpointCurrencyAmount(base_currency, endpoint_currency) {
+    var response = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${base_currency}.json`)
+    var data = await response.json()
+    data = JSON.parse(data)
+    return data[`${base_currency}`][`${endpoint_currency}`];
+
 }
+
+
+var base_currency = "eth";
+var endpoint_currency = "rub";
+var amount = getEndpointCurrencyAmount(base_currency, endpoint_currency)
+console.log(amount)
